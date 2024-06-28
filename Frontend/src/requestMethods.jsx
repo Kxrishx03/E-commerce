ada7b4efb82dd9a264e83c1a279522eb2dcd97c0
Kxrishx03 <2001karishmamandal@gmail.com>
2024-06-28 18:32:31 +0530

    "DONE"

--- Frontend/src/requestMethods.jsx
+++ Frontend/src/requestMethods.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api/";
+// const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = "https://e-commerce-pi-steel.vercel.app/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2N2I5M2NjNTkzYjQzY2I5MmQwNWE4MiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcxOTM3NDk1MywiZXhwIjoxNzE5ODA2OTUzfQ.rZza2alaLxJ0g7xZBwIQ5G9l9ajzpwFIGfAe_nfLiKA" ;
 
 export const publicRequest = axios.create({
